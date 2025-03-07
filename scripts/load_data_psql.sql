815aacd0cb92a2d9d5461bcb791a6d8c72f9ce4c
dsomni <pro100pro10010@gmail.com>
2025-03-07 21:22:48 +0300

    Add [postgres loading] scripts

--- scripts/load_data_psql.sql
+++ scripts/load_data_psql.sql
@@ -1,5 +1,12 @@
 START TRANSACTION;
 
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS campaigns;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS clients;
+
 CREATE TABLE IF NOT EXISTS friends (
 	friend1 integer NOT NULL,
 	friend2 integer NOT NULL,
@@ -7,9 +14,9 @@ CREATE TABLE IF NOT EXISTS friends (
 );
 
 CREATE TABLE IF NOT EXISTS campaigns (
-	id integer NOT NULL,
-	campaign_type varchar(16) NOT NULL,
-	channel varchar(12),
+	campaign_id integer NOT NULL,
+	campaign_type varchar(32) NOT NULL,
+	channel varchar(16),
 	topic varchar(64),
 	started_at timestamp WITHOUT TIME ZONE,
 	finished_at timestamp WITHOUT TIME ZONE,
@@ -26,39 +33,37 @@ CREATE TABLE IF NOT EXISTS campaigns (
 	subject_with_saleout boolean,
 	is_test boolean,
 	position integer,
-	PRIMARY KEY (id, campaign_type)
+	PRIMARY KEY (campaign_id, campaign_type)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+	product_id integer NOT NULL,
+	brand varchar(32),
+	price real,
+	category_id bigint,
+	category_code text,
+	CONSTRAINT unique_product PRIMARY KEY (product_id)
 );
 
 CREATE TABLE IF NOT EXISTS events (
 	event_time timestamp WITH TIME ZONE,
-	event_type varchar(8),
+	event_type varchar(16),
 	product_id integer,
-	category_id bigint,
-	category_code varchar(40),
-	brand varchar(30),
 	price real,
 	user_id integer,
-	user_session varchar(36)
-);
-
-CREATE TABLE IF NOT EXISTS client_first_purchase_date (
-	client_id bigint PRIMARY KEY,
-	first_purchase_date date,
-	user_id integer,
-	user_device_id smallint
+	user_session varchar(36),
+	CONSTRAINT fk_products_events FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
 
 CREATE TABLE IF NOT EXISTS messages (
-	id bigint PRIMARY KEY,
-	message_id varchar(36),
+	message_id varchar(36) NOT NULL,
 	campaign_id integer,
-	message_type varchar(16),
+	message_type varchar(32),
 	client_id bigint,
-	channel varchar(12),
+	channel varchar(16),
 	category varchar(16),
-	platform varchar(10),
-	email_provider varchar(32),
-	stream varchar(7),
+	platform varchar(16),
+	stream varchar(16),
 	date date,
 	sent_at timestamp WITHOUT TIME ZONE,
 	is_opened boolean,
@@ -81,15 +86,23 @@ CREATE TABLE IF NOT EXISTS messages (
 	purchased_at timestamp WITHOUT TIME ZONE,
 	created_at timestamp WITHOUT TIME ZONE,
 	updated_at timestamp WITHOUT TIME ZONE,
-	user_device_id smallint,
-	user_id integer,
-	CONSTRAINT fk_campaigns_to_messages FOREIGN KEY (campaign_id, message_type) REFERENCES campaigns (id, campaign_type)
+	CONSTRAINT unique_message PRIMARY KEY (message_id),
+	CONSTRAINT fk_campaigns_messages FOREIGN KEY (campaign_id, message_type) REFERENCES campaigns (campaign_id, campaign_type)
 );
 
-\COPY campaigns from './data/cleaned/campaigns.csv' delimiter ',' CSV header null as '';
-\COPY client_first_purchase_date from './data/cleaned/client_first_purchase_date.csv' delimiter ',' CSV header null as '';
-\COPY events from './data/cleaned/events.csv' delimiter ',' CSV header null as '';
-\COPY friends from './data/cleaned/friends.csv' delimiter ',' CSV header null as '';
-\COPY messages from './data/cleaned/messages.csv' delimiter ',' CSV header null as '';
+CREATE TABLE IF NOT EXISTS clients (
+	client_id bigint PRIMARY KEY,
+	user_id integer,
+	user_device_id smallint,
+	first_purchase_date date,
+	email_provider varchar(64)
+);
+
+\COPY products from './data/postgres/products.csv' delimiter ',' CSV header null as '';
+\COPY clients from './data/postgres/clients.csv' delimiter ',' CSV header null as '';
+\COPY campaigns from './data/postgres/campaigns.csv' delimiter ',' CSV header null as '';
+\COPY messages from './data/postgres/messages.csv' delimiter ',' CSV header null as '';
+\COPY events from './data/postgres/events.csv' delimiter ',' CSV header null as '';
+\COPY friends from './data/postgres/friends.csv' delimiter ',' CSV header null as '';
 
 commit;
